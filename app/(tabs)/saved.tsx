d5516bcba7888821513d9b1213edb7610479aaa8
Rok Rihar <rok.rihar123@gmail.com>
2025-11-14 20:38:00 +0100

    Updated saved page to have meals, and turned displayed days into buttons.

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StatusBar, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const DATA = [
@@ -14,13 +14,11 @@ const DATA = [
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
     padding: 10,
     marginTop: StatusBar.currentHeight || 0,
   },
   row:{
-    flexDirection: "row", // put children side by side
+    flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
     width: "100%",
@@ -31,7 +29,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
     borderRadius: 8,
     marginHorizontal: 4,
-    height: '8%'
   },
   cardText: {
     fontSize: 24,
@@ -40,9 +37,52 @@ const styles = StyleSheet.create({
     fontSize: 36,
     marginVertical: 20,
   },
+  countText: {
+    color: '#FF00FF',
+  },
+  containerCount: {
+    flex: 1,
+    
+  },
+  button: {
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  countContainer: {
+    display: "flex",
+    padding: 10,
+  },
+  addSubtract: {
+    backgroundColor: "#8c8d8eff",
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderRadius: 8,
+    marginHorizontal: 4,
+    width: 30,
+  },
+   mealContainer: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: "#8c8d8eff",
+  },
+  mealText: {
+    fontWeight: "bold",      // ne fontStyle, ampak fontWeight
+    fontSize: 28,
+  },
+  foodText: {
+    fontWeight: "bold",
+    color: "green",          // tintColor je za <Image />, ne za <Text />
+    fontSize: 28,
+  },
 });
 
+const items = ["Meal", "food",];
+
 const saved = () => {
+  const [count, setCount] = useState(0);
+  const addOnPress = () => setCount(count + 1);
+  const subtractOnPress = () => setCount(count - 1);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
@@ -51,17 +91,65 @@ const saved = () => {
         <FlatList
           data={DATA}
           renderItem={({ item }) => (
-            <View style={styles.card}>
-              <Text style={styles.cardText}>{item.title}</Text>
-            </View>
+            <TouchableHighlight >
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item.title}</Text>
+              </View>
+            </TouchableHighlight>
           )}
           keyExtractor={(item) => item.id}
           horizontal
           showsHorizontalScrollIndicator={false}
         />
+
+        {/*
+        TO LAHK DODAVA ČE BI RES HOTLA
+        <View style={styles.countContainer}>
+          <Text>Number of meals: {count}</Text>
+          <TouchableHighlight >
+            <View >
+              <Text style={styles.addSubtract} onPress={addOnPress}>+</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight>
+            <View >
+              <Text style={styles.addSubtract} onPress={subtractOnPress}>-</Text>
+            </View>
+          </TouchableHighlight>
+        </View> */}
+        
+        <View style={styles.mealContainer}>
+          <Text style={styles.mealText}>Breakfast</Text>
+          <TouchableHighlight>
+            <Text style={styles.foodText}>Add food</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.mealContainer}>
+          <Text style={styles.mealText}>Lunch</Text>
+          <TouchableHighlight>
+            <Text style={styles.foodText}>Add food</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.mealContainer}>
+          <Text style={styles.mealText}>Dinner</Text>
+          <TouchableHighlight>
+            <Text style={styles.foodText}>Add food</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.mealContainer}>
+          <Text style={styles.mealText}>Snacks</Text>
+          <TouchableHighlight>
+            <Text style={styles.foodText}>Add food</Text>
+          </TouchableHighlight>
+        </View>
+          
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
+
 
 export default saved;
